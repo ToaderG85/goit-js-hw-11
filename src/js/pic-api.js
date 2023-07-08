22cb6fc71658482dcb5562c2a84012d1bfe89323
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-07-08 15:23:55 +0300

    update

--- src/js/pic-api.js
+++ src/js/pic-api.js
@@ -2,17 +2,27 @@ const ENDPOINT = 'https://pixabay.com/api/';
 const API_KEY = '38102784-37e9ad2cc652dbc0da2d9323c';
 
 
-async function getImages(query, page, perPage) {
-  try {
-    const response = await fetch(`${ENDPOINT}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
-    if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-  } catch (error) {
-    console.error;
+// async function getImages(query, page, perPage) {
+//   try {
+//     const response = await fetch(`${ENDPOINT}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`);
+//     if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//   } catch (error) {
+//     console.error;
     
-  }
+//   }
+// }
+
+async function getImages(query, page, perPage) {
+  return fetch(`${ENDPOINT}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
 export default {getImages};
